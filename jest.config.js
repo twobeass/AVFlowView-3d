696c034be290c2e2afbb1ce8571812e3e6f43f7c
twobeass <t.nickels@gmail.com>
2025-11-16 13:05:24 +0100

    chore: add Jest configuration for ESM module support

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+export default {
+  testEnvironment: 'node',
+  transform: {},
+  moduleNameMapper: {
+    '^(\\.\\.?\\/.+)\\.js$': '$1',
+  },
+  extensionsToTreatAsEsm: ['.js'],
+  testMatch: [
+    '**/tests/**/*.test.js',
+    '**/__tests__/**/*.js',
+  ],
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/main.js',
+    '!src/**/*.test.js',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  coverageDirectory: 'coverage',
+  verbose: true,
+};
